338393068ad5198802ac0858441b168d6b709bb0
Q_S_Y_Q <charlesxu86@163.com>
2018-01-23 10:18:38 +0800

    添加项目分类文件夹

---
+++ src/Sougou/__init__.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: __init__.py.py
+@time: 2018/1/23 10:17
+@desc:
+"""
+
+if __name__ == '__main__':
+    pass
---
+++ src/StateGrid/Create_features_A.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 1.0
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: Create_features_A.py
+@time: 2018/1/23 10:15
+@desc: 根据数据构建特征
+"""
+
+if __name__ == '__main__':
+    pass
