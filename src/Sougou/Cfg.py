3965f41700e3fb984181423cf420ef8d250ede31
Q_S_Y_Q <charlesxu86@163.com>
2018-02-02 14:38:57 +0800

    配置文件

---
+++ src/Sougou/Cfg.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: Cfg.py
+@time: 2018/2/2 14:37
+@desc: 配置信息
+"""
+
+data_path = 'E:\py_workspace\\UserProfile\data\Sougou'
+
