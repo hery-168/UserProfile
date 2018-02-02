e1aebc5f3a4fbae062aa557d20b3cec7d10f11dc
Q_S_Y_Q <charlesxu86@163.com>
2018-02-02 16:43:46 +0800

    构建用户消费行为特征

---
+++ src/StateGrid/Create_features_C.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: Create_features_C.py
+@time: 2018/1/24 14:38
+@desc: 构建用户消费行为特征
+"""
+
+if __name__ == '__main__':
+    pass
