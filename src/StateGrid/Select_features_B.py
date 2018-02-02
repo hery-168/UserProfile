15b5b9fba6f4b4be89ed10ae6ad66168d9044488
Q_S_Y_Q <charlesxu86@163.com>
2018-02-02 17:00:34 +0800

    采用xgboost选择高敏感度用户的文本特征

---
+++ src/StateGrid/Select_features_B.py
@@ -0,0 +1,13 @@
+#-*- coding:utf-8 _*-
+
+"""
+@version: 
+@author: CharlesXu
+@license: Q_S_Y_Q 
+@file: Select_features_B.py
+@time: 2018/2/2 16:49
+@desc: 采用xgboost选择高敏感度用户的文本特征
+"""
+
+if __name__ == '__main__':
+    pass
